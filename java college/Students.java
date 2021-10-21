e1af570aebe6f31d4665d16dad4cd3dc9339a540
Shubham <skumarroyktr@gmail.com>
2021-10-21 09:13:32 +0530

    i m trying to save another file

---
+++ java college/Students.java
@@ -0,0 +1,24 @@
+public class Students {
+        String AdmissionID ;
+        String name;
+        String Collegename;
+        String Branch;
+        String Address;
+        float Fees;
+        int Rollno;
+        char Sec;
+        char Gender;
+        int MobielNo;
+        boolean Passout;
+
+
+        public static String Name(){
+            String name = "Shubham kumar roy";
+            System.out.println("My name is");
+            return name;
+        }
+            public static void main(String[] args) {
+                System.out.println(Name());
+            }
+        }
+
